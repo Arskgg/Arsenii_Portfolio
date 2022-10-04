3c6fd6a26fccc6aa7ed235f4b44ca4ff98d03b83
Arsenii <arsenyper@gmail.com>
2022-10-03 20:24:54 -0500

    Made divider wider

--- components/Divider/index.js
+++ components/Divider/index.js
@@ -4,8 +4,8 @@ const Divider = ({ vertical, ...props }) => {
       {...props}
       style={{
         backgroundColor: "var(--secondary-color)",
-        height: vertical ? "100%" : "0.1px",
-        width: vertical ? "0.1px" : "100%",
+        height: vertical ? "100%" : "1px",
+        width: vertical ? "1px" : "100%",
       }}
     />
   );
